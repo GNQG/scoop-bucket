6c7a0bb16610e627c3e76b5eb22380c414113079
GNQG <my.t.gnq+dev@gmail.com>
2019-04-08 00:47:01 +0900

    checkver.ps1: support command line options for original checkver.ps1

--- checkver.ps1
+++ checkver.ps1
@@ -1,4 +1,19 @@
-$scooppath = scoop which scoop
+param(
+    [String] $App = '*',
+    [ValidateScript( {
+            if (!(Test-Path $_ -Type Container)) {
+                throw "$_ is not a directory!"
+            }
+            $true
+        })]
+    [String] $Dir = "$PSScriptRoot",
+    [Switch] $Update,
+    [Switch] $ForceUpdate,
+    [Switch] $SkipUpdated,
+    [String] $Version = ""
+)
+
+$scooppath = scoop.ps1 which scoop
 $scooppath_bin = Split-Path -Parent $scooppath
 $scooppath_lib = Resolve-Path "$scooppath_bin\..\lib"
 
@@ -7,15 +22,24 @@ $checkver = "$scooppath_bin\checkver.ps1"
 . "$scooppath_lib\autoupdate.ps1"
 . "$scooppath_lib\manifest.ps1"
 
-Get-ChildItem -LiteralPath "." -Filter "*.json" -File | ForEach-Object {
-    $checksum_orig = (Get-FileHash $_).Hash
-    &$checkver -Dir . $_.BaseName -Update
-    $checksum_new = (Get-FileHash $_).Hash
-    if ($checksum_orig -ne $checksum_new) {
-        $manifest = Get-Content -Raw $_ | ConvertFrom-Json
-        $version_table = get_version_substitutions $manifest.version $null
+if ($Update -or $ForceUpdate) {
+    $current_hash = @()
 
-        update_manifest_prop "bin" $manifest $version_table
-        [System.IO.File]::WriteAllLines($_, (ConvertToPrettyJson $manifest))
+    Get-ChildItem -LiteralPath $Dir -Filter "$App.json" | ForEach-Object {
+        $current_hash += @{Name=$_.Name ; Hash= (Get-FileHash $_).Hash}
+    }
+}
+
+&$checkver -Dir:$Dir -App:$App -Update:$Update -ForceUpdate:$ForceUpdate `
+    -SkipUpdated:$SkipUpdated -Version:$Version
+
+if ($Update -or $ForceUpdate) {
+     $current_hash | ForEach-Object {
+        if ($ForceUpdate -or ((Get-FileHash $_.Name).Hash -ne $_.Hash )) {
+            $manifest = Get-Content -Raw $_.Name | ConvertFrom-Json
+            $version_table = get_version_substitutions $manifest.version $null
+            update_manifest_prop "bin" $manifest $version_table
+            [System.IO.File]::WriteAllLines($_, (ConvertToPrettyJson $manifest))
+        }
     }
 }
